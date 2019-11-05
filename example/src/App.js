bd725c36bf74eec25886b847390b97c98293d17e
Archie Reyes <archbrys@gmail.com>
2019-11-06 01:26:43 +0800

    Render input base on field type

--- example/src/App.js
+++ example/src/App.js
@@ -4,9 +4,25 @@ import ExampleComponent from 'react-dy-forms'
 
 export default class App extends Component {
   render () {
+    const fields = [
+      {
+        label : "First Name",
+        placeholder : "",
+        defaultValue : "test",
+        type: "text",
+        isRequired: true
+      },
+      {
+        label : "Last Name",
+        placeholder : "lastname",
+        // defaultValue : "test",
+        type: "text",
+        isRequired: true
+      }
+    ]
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <ExampleComponent fields={fields} />
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,42 @@
+// Imports: Dependencies
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
+//Imports: Styles
 import styles from './styles.css'
 
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
-  }
 
-  render() {
-    const {
-      text
-    } = this.props
+const Input = ({input}) => (
+  <div className={styles.inputGroup}>
+    <label>{input.label}</label>
+    <input type={input.type} placeholder={input.placeholder} defaultValue={input.defaultValue}/>
+  </div>
+)
 
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
-  }
+
+const DyForms = (props) => {
+  console.log(props.fields)
+  // Return null if field is not an array
+  if (!Array.isArray(props.fields)) return null;
+
+  return (
+    <form className={styles.formStyle}>
+      {props.fields.map((input, i) => {
+        return <Input key={`dy-${i}`} input={input}/>
+      })}
+    </form>
+  )
 }
+
+DyForms.propTypes  = {
+  fields : PropTypes.arrayOf(PropTypes.exact({
+    label : PropTypes.string.isRequired,
+    placeholder : PropTypes.string,
+    defaultValue : PropTypes.string,
+    type: PropTypes.string.isRequired,
+    isRequired:  PropTypes.bool
+  })).isRequired,
+}
+
+//Export 
+export default DyForms;
